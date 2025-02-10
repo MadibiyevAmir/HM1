b70c86555345955a2b955b30868a9dff7a12a007
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-02-10 23:35:48 +0500

    ScoreManager.java

---
+++ score/ScoreManager.java
@@ -0,0 +1,13 @@
+package score;
+
+public class ScoreManager {
+    private int score;
+
+    public void addScore(int points) {
+        score += points;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
