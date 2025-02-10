ff74c797567f3e27b3fbaef0f5085cd676c95caf
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-02-10 23:26:47 +0500

    Warrior.java

---
+++ characters/Warrior.java
@@ -0,0 +1,8 @@
+package characters;
+import items.IWeapon;
+
+public class Warrior extends Character {
+    public Warrior(String name, IWeapon weapon) {
+        super(name, 150, weapon);
+    }
+}
